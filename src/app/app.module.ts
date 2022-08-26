0889e50fa9c5ea2645f37add02de4031679de69d
anna7461 <a.ch7461@gmail.com>
2022-08-26 13:48:36 +0400

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { SharedComponentsComponent } from './shared-components/shared-components.component';
+import { QuantityInputComponent } from './shared-components/forms/quantity-input/quantity-input.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, SharedComponentsComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    SharedComponentsComponent,
+    QuantityInputComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
